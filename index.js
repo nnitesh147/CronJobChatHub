ff98aaf544ffdde7574f7d14a80f7cc97de2ce4d
nnitesh147 <nniteshprasad147@gmail.com>
2024-10-02 19:48:53 +0530

    main

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { CronJob } from "cron";
+import axios from "axios";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Cron Job Server is running");
+});
+
+// Define the task function
+const task = async () => {
+  const date = new Date();
+
+  const { data } = await axios.get("https://chathubserver-cgut.onrender.com");
+
+  console.log(`${data} at ${date}`);
+};
+
+// Create the cron job, running every 20 minutes
+const job = new CronJob("0 */5 * * * *", task);
+
+// Start the job
+job.start();
+app.listen(3000, () => {
+  console.log(`Server is running on 3000 Port`);
+});
