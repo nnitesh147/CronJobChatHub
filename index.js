c8f876dfc97b49c17fbf12ec841505a1176dd2c9
nnitesh147 <nniteshprasad147@gmail.com>
2025-06-25 23:25:37 +0530

    started-job

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.get("/", (req, res) => {
 
 // Define the task function
 const task = async () => {
-  const { data } = await axios.get("https://chathubserver-cgut.onrender.com");
+  const { data } = await axios.get("https://chathubserver-cgut.onrender.com/");
   console.log(new Date() + "     " + data);
 };
 
 // Create the cron job, running every 10 minutes
-const job = new CronJob("0 */5 * * * *", task);
+const job = new CronJob("*/10 * * * * *", task);
 // Start the job
 job.start();
 
