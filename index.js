775d49165d1ace7bcfc4809e94486e1e28a45f4b
nnitesh147 <nniteshprasad147@gmail.com>
2025-06-25 23:00:21 +0530

    started-job

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const task = async () => {
   const { data } = await axios.get("https://chathubserver-cgut.onrender.com");
   console.log(new Date() + "     " + data);
 };
+
 // Create the cron job, running every 10 minutes
 const job = new CronJob("0 */5 * * * *", task);
 // Start the job
-//job.start();
+job.start();
+
 app.listen(3000, () => {
   console.log(`Server is running on 3000 Port`);
 });
